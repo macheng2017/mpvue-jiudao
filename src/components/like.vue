e8aaaca113c7c3de0753cbdc31f96da252ca5bad
macheng2017 <gos360@163.com>
2019-01-19 17:46:37 +0800

    add heart icon

---
+++ src/components/like.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="container">
+    <img src="../components/img/like@dis.png" alt="">
+    <span>999</span>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+  .container{
+    display:flex;
+    flex-direction:row;
+    padding:10rpx;
+  }
+  .container img {
+    width:32rpx;
+    height:28rpx;
+  }
+  .container span{
+    font-family: "PingFangSC-Thin";
+    font-size: 24rpx;
+    position: relative;
+    bottom: 10rpx;
+    left: 10rpx;
+    line-height: 24rpx;
+  }
+
+</style>
---
+++ src/pages/classic/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <like></like>
+    <div>123</div>
+  </div>
+</template>
+
+<script>
+import like from '@/components/like'
+export default {
+  data () {
+    return {}
+  },
+  components: {
+    like
+  }
+}
+</script>
+
+<style>
+</style>
