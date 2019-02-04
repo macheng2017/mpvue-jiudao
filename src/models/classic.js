9670a2107eee3606d8653f0ca8fda8a749464946
mac <gos360@163.com>
2019-02-04 11:18:30 +0800

    add storage

--- src/models/classic.js
+++ src/models/classic.js
@@ -8,20 +8,40 @@ class ClassicModel extends Http {
       success: res => {
         callBack(res)
         this._setLatestIndex(res.index)
+        let key = this._getKey(res.index)
+        wx.setStorageSync(key, res)
       }
     })
   }
   // 获取当前一期的上一期(默认/初始化显示的是最新一期)
   getClassic(index, previousOrNext, callBack) {
-    this.request({
-      url: `/classic/${index}/${previousOrNext}`,
-      method: 'GET',
-      success: res => {
-        callBack(res)
-      }
-    })
+    // 加入缓存
+    // 1. 请求数据的时候先去查找缓存
+    // 2. 如果缓存不存在则,发送请求到服务器,获取到数据返回并加入缓存
+    // 3. 如果缓存存在直接返回即可
+    let key =
+      previousOrNext === 'next'
+        ? this._getKey(index + 1)
+        : this._getKey(index - 1)
+    let val = wx.getStorageSync(key)
+    console.log('object111', val)
+    if (!val) {
+      console.log(key)
+      this.request({
+        url: `/classic/${index}/${previousOrNext}`,
+        method: 'GET',
+        success: res => {
+          callBack(res)
+          wx.setStorageSync(key, res)
+        }
+      })
+    } else {
+      callBack(wx.getStorageSync(key))
+    }
   }
-
+  _getKey(index) {
+    return 'classic-' + index
+  }
   isFirst(index) {
     return index === 2 ? 1 : 0
   }
