442d54dbe12081f455f353fa6d639c8e5dd4b85d
macheng2017 <gos360@163.com>
2019-02-25 10:40:24 +0800

    apply change for mac1

--- src/pages/bookDetail/index.vue
+++ src/pages/bookDetail/index.vue
@@ -17,12 +17,54 @@
       <div class="headline">内容</div>
       <div class="content">{{summary}}</div>
     </div>
+    <div class="sub-container">
+      <div class="headline">书本信息</div>
+      <div class="detail-container">
+        <div class="vertical description">
+          <div>出版社</div>
+          <div>出版年</div>
+          <div>页数</div>
+          <div>定价</div>
+          <div>装帧</div>
+        </div>
+        <div class="vertical">
+          <div>{{book.publisher}}</div>
+          <div>{{book.pubdate}}</div>
+          <div>{{book.pages}}</div>
+          <div>{{book.price}}</div>
+          <div>{{book.binding}}</div>
+        </div>
+      </div>
+    </div>
+    <div class="post-container">
+      <div @click="onFakePost" class="post-fake">
+        <div>输入短评</div>
+      </div>
+      <div class="like-container">
+        <div class="like">
+          <Like :like="likeStatus" :count="likeCount" @on-like="onLike"></Like>
+        </div>
+      </div>
+    </div>
+
+    <!-- 隐藏层 -->
+    <div class="posting-container">
+      <div class="post-header">
+        <div>标签</div>
+        <div class="cancel">取消</div>
+      </div>
+      <div class="comment-container"></div>
+      <input type="text" placeholder="最多输入12个字" class="post">
+    </div>
   </div>
 </template>
 
 <script>
 import BookModel from '@/models/book'
 import Tag from '@/components/tag'
+import Like from '@/components/like'
+import LikeModel from '@/models/like'
+let likeModel = new LikeModel()
 
 const bookModel = new BookModel()
 export default {
@@ -32,15 +74,13 @@ export default {
       comments: [],
       book: {},
       likeStatus: false,
-      likeCount: 0
-      // summary: ''
+      likeCount: 0,
+      summary: '',
+      posting: false
     }
   },
-  computed: {
-    summary() {}
-  },
   // externalClasses: ['tag-class'],
-  components: { Tag },
+  components: { Tag, Like },
   methods: {
     async getBooksData(bid) {
       this.book = await bookModel.getDetail(bid)
@@ -52,7 +92,22 @@ export default {
       this._formatContent()
       console.log(this.summary)
     },
-    _formatContent() {}
+    _formatContent() {
+      console.log('_formatContent')
+      let summary = this.book.summary
+      const reg = new RegExp('\\\\n', 'g')
+      this.summary = summary.replace(reg, '\n')
+    },
+    onLike(event) {
+      likeModel.like({
+        like: event.like, // 用于标注点赞的状态，点赞/取消点赞
+        art_id: this.book.id,
+        type: 400
+      })
+    },
+    onFakePost(event) {
+      this.posting = true
+    }
   },
   mounted() {
     const bid = this.$root.$mp.query.bid
@@ -122,6 +177,87 @@ export default {
   background-color: #eefbff;
 }
 .content {
-  text-indent: 100rpx;
+  text-indent: 56rpx;
+  font-size: 28rpx;
+}
+.vertical {
+  display: flex;
+  flex-direction: column;
+}
+
+.description {
+  color: #999;
+  margin-right: 30rpx;
+}
+.detail-container {
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-start;
+  margin-bottom: 100rpx;
+  font-size: 28rpx;
+  color: #666;
+}
+
+.post-container {
+  height: 100rpx;
+  box-shadow: 1px -1px 1px #e3e3e3;
+  position: fixed;
+  width: 690rpx;
+  background-color: #fff;
+  bottom: 0;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  padding: 0 30rpx;
+  justify-content: space-between;
+}
+
+.post-fake {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  height: 60rpx;
+  width: 460rpx;
+  border: 1px solid #999;
+  border-radius: 15px;
+  font-size: 22rpx;
+  padding-left: 20rpx;
+}
+
+.like {
+  margin-right: 30rpx;
+  margin-top: 10rpx;
+}
+.like > div {
+  background-color: #fff;
+}
+
+.like-container {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.post-container {
+  bottom: 0;
+  position: fixed;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background-color: #fff;
+  width: 100%;
+  z-index: 999;
+}
+.post-header {
+  width: 100%;
+  border-bottom: 1px solid #f5f5f5;
+  border-top: 1px solid #f5f5f5;
+  height: 100rpx;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
 }
 </style>
